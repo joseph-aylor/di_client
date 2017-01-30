a31dddfa69e3605be031d2833c56b1e2d52d9674
Joe A <jaylor@topicdesign.com>
2017-01-30 13:27:59 -0500

    create pls files for itunes

--- load.rb
+++ load.rb
@@ -7,14 +7,32 @@ class Station
   attr_accessor :title
   def initialize(url, title)
     @url = url
+    @readable_title = title
     @title = title_format(title)
     create_alias
+    create_pls
   end
 
   private
 
   def create_alias
     puts "alias #{@title}=\"mplayer '#{@url}'\""
+  end
+
+  def create_pls
+    File.open("#{title}.pls", "w") do |f|
+      f.write(pls_file_text)
+      f.close
+    end
+  end
+
+  def pls_file_text
+    pls_template = "[playlist]\n"
+    pls_template += "NumberOfEntries=1\n"
+    pls_template += "File1=#{@url}\n"
+    pls_template += "Title1=Digitally Imported - #{@readable_title}\n"
+    pls_template += "Length1=-1\n"
+    pls_template += "Version=2"
   end
 
   def title_format(title)
